fb401b810f5710f3254bc01c627c845b57f9a637
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2024-05-22 20:50:03 +0700

    feat: implement fetchContent

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
-import './App.css';
+import "./App.css";
 
-import { Controls } from './components/Controls';
-import { CurrentlyReading } from './components/CurrentlyReading';
+import { Controls } from "./components/Controls";
+import { CurrentlyReading } from "./components/CurrentlyReading";
+import { fetchContent } from "./lib/content";
 
 function App() {
-  // const [sentences, setSentences] = useState<Array<string>>([]);
-  // const { currentWord, currentSentence, controls } = useSpeech(sentences);
+	// const [sentences, setSentences] = useState<Array<string>>([]);
+	// const { currentWord, currentSentence, controls } = useSpeech(sentences);
 
-  return (
-    <div className="App">
-      <h1>Text to speech</h1>
-      <div>
-        <CurrentlyReading/>
-      </div>
-      <div>
-        <Controls/>
-      </div>
-    </div>
-  );
+	fetchContent();
+
+	return (
+		<div className="App">
+			<h1>Text to speech</h1>
+			<div>
+				<CurrentlyReading />
+			</div>
+			<div>
+				<Controls />
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/lib/content.ts
+++ src/lib/content.ts
@@ -1,10 +1,25 @@
 const API_URL = "http://localhost:5174/content";
 
+type Content = {
+	content: string;
+};
+
 /**
  * Fetch the content from the api
  * In case of an error, return content as "<speak><s>There was an error</s></speak>"
  */
-const fetchContent = async (url = API_URL): Promise<string> => {};
+const fetchContent = async (url = API_URL): Promise<string> => {
+	try {
+		const res = await fetch(url);
+
+		const response = (await res.json()) as Content;
+
+		return response.content;
+	} catch (e) {
+		console.error(e);
+		return e as string;
+	}
+};
 
 /**
  * Parse the content into sentences, and return an array of sentences. Look at the Readme for sample input and expected output.
